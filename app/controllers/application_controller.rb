549376f6244988be03c6aa294e24f952544626ac
Dakota Martinez <dakotaleemusic@gmail.com>
2018-07-31 14:37:44 -0700

    set up users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+
+  def current_user 
+    @current_user ||= User.first_or_create(
+      first_name: "Chris",
+      last_name: "Dundon",
+      unit: "Basement",
+      manager: true
+    )
+  end
 end
