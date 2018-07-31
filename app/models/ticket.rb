343bc87ac9d689564224584c7310220c02167c8a
Dakota Martinez <dakotaleemusic@gmail.com>
2018-07-31 15:29:50 -0700

    demo renaming a has many through association

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ApplicationRecord
   belongs_to :unit
   has_many :assigned_tickets
-  has_many :employees, through: :assigned_tickets
+  has_many :assigned_employees, through: :assigned_tickets, source: 'employee'
 end
