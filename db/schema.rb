37c06deeae785b9865592615388bd5d0f5dbc69a
Dakota Martinez <dakotaleemusic@gmail.com>
2018-07-31 14:42:04 -0700

    add tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_213941) do
+ActiveRecord::Schema.define(version: 2018_07_31_214100) do
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "employee_id"
+    t.integer "unit_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "units", force: :cascade do |t|
     t.string "building_name"
